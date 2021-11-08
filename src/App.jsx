679d607e5db191048f04f728e00f01424c86d91d
John Tsaava <johntsaava@gmail.com>
2021-11-08 21:42:10 +0400

    vite setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export const App = () => {
+  return (
+    <main className="App">
+      <h1 className="text-xl">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis,
+        dolorum.
+      </h1>
+      <p className="mt-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+        ratione velit, suscipit quibusdam vero hic quaerat quam nihil nam ipsam
+        quo repellat odit deleniti debitis voluptates tempore maiores provident
+        aut.
+      </p>
+    </main>
+  );
+};
