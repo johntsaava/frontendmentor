7cd4ca092dc30d6e45b99c431de13fd9d3b1be32
John Tsaava <johntsaava@gmail.com>
2021-11-08 23:12:20 +0400

    add sunnyside agency layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
 export const App = () => {
   return (
-    <main>
-      <h1 className="text-xl">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis,
-        dolorum.
-      </h1>
-      <p className="mt-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-        ratione velit, suscipit quibusdam vero hic quaerat quam nihil nam ipsam
-        quo repellat odit deleniti debitis voluptates tempore maiores provident
-        aut.
-      </p>
+    <main className="grid grid-cols-1 sm:grid-cols-2">
+      <section className="col-span-full h-[80vh] bg-header bg-contain bg-no-repeat bg-bottom bg-cyan-400">
+        <h1 className="font-display text-6xl uppercase tracking-widest text-white text-center mt-16">
+          We are creatives
+        </h1>
+      </section>
+      <section className="">
+        <h2>Transform your brand</h2>
+        <p>
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </p>
+        <a href="#">Learn more</a>
+      </section>
+      <figure className="">Figure</figure>
+      <figure className="">Figure</figure>
+      <section className="">
+        <h2>Stand out to the right audience</h2>
+        <p>
+          Using a collaborative formula of designers, researchers,
+          photographers, videographers, and copywriters, we’ll build and extend
+          your brand in digital places.
+        </p>
+        <a href="#">Learn more</a>
+      </section>
+      <section className="">
+        <h3>Graphic design</h3>
+        <p>
+          Great design makes you memorable. We deliver artwork that underscores
+          your brand message and captures potential clients’ attention.
+        </p>
+      </section>
+      <section className="">
+        <h3>Photography</h3>
+        <p>
+          Increase your credibility by getting the most stunning, high-quality
+          photos that improve your business image.
+        </p>
+      </section>
+      <section className="col-span-full">Section</section>
+      <figure className="">Figure</figure>
+      <figure className="">Figure</figure>
+      <figure className="">Figure</figure>
+      <figure className="">Figure</figure>
+      <footer className="col-span-full">Section</footer>
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,44 @@
 module.exports = {
+  mode: "jit",
   purge: {
     content: ["./src/**/*.jsx", "./index.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['"Fraunces"', "serif"],
+        body: ['"Barlow"', "sans-serif"],
+      },
+      backgroundImage: {
+        header: "url('../src/images/desktop/image-header.jpg')",
+      },
+    },
+    colors: {
+      red: {
+        DEFAULT: "hsl(7, 99%, 70%)",
+      },
+      yellow: {
+        DEFAULT: "hsl(51, 100%, 49%)",
+      },
+      cyan: {
+        400: "hsl(200, 100%, 62%)",
+        600: "hsl(198, 62%, 26%)",
+        900: "hsl(167, 40%, 24%)",
+      },
+      blue: {
+        600: "hsl(198, 62%, 26%)",
+        900: "hsl(212, 27%, 19%)",
+      },
+      "grayish-blue": {
+        400: "hsl(210, 4%, 67%)",
+        700: "hsl(232, 10%, 55%)",
+        900: "hsl(213, 9%, 39%)",
+      },
+      white: {
+        DEFAULT: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   variants: {
     extend: {},
